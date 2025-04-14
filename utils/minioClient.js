29714759a58d80d7cd1cbb23b2bd7f5738b01e5f
Mirzamurod <rahimberdiyevmirzamurod@gmail.com>
2025-04-14 19:10:57 +0500

    changed minio client

--- utils/minioClient.js
+++ utils/minioClient.js
@@ -1,11 +1,13 @@
 import { Client } from 'minio'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const minioClient = new Client({
-  endPoint: process.env.MINIO_ENDPOINT || '127.0.0.1',
+  endPoint: process.env.MINIO_ENDPOINT,
   port: parseInt(process.env.MINIO_PORT) || 9000,
   useSSL: false,
-  accessKey: process.env.MINIO_ACCESS_KEY || 'minioadmin',
-  secretKey: process.env.MINIO_SECRET_KEY || 'minioadmin',
+  accessKey: process.env.MINIO_ACCESS_KEY,
+  secretKey: process.env.MINIO_SECRET_KEY,
 })
 
 export default minioClient
