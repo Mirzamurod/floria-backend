911686c835e27dd1fa9865aadfd4942ff4aff331
Mirzamurod <rahimberdiyevmirzamurod@gmail.com>
2025-04-14 21:53:54 +0500

    changed minio client

--- utils/minioClient.js
+++ utils/minioClient.js
@@ -5,7 +5,7 @@ dotenv.config()
 const minioClient = new Client({
   endPoint: process.env.MINIO_ENDPOINT,
   port: parseInt(process.env.MINIO_PORT) || 9000,
-  useSSL: false,
+  useSSL: process.env.PROD === 'development' ? false : true,
   accessKey: process.env.MINIO_ACCESS_KEY,
   secretKey: process.env.MINIO_SECRET_KEY,
 })
