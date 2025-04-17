dc588bac3dfad454d12402df7ae248e6b5f55466
Mirzamurod <rahimberdiyevmirzamurod@gmail.com>
2025-04-17 08:00:59 +0500

    changed presigned url time

--- utils/presignedUrl.js
+++ utils/presignedUrl.js
@@ -5,7 +5,8 @@ const getPresignedUrl = async filename => {
   const url = await minioClient.presignedGetObject(
     bucketName, // Bucket nomi
     filename, // Fayl nomi (to‘liq path bilan, masalan: audios/abc.mp3)
-    60 * 5 // URL amal qilish vaqti (sekundlarda) – bu yerda 1 daqiqa
+    // 60 * 5 // URL amal qilish vaqti (sekundlarda) – bu yerda 1 daqiqa
+    24 * 60 * 60
   )
 
   // return url.replace(':9000', '').replace('http://', 'https://')
